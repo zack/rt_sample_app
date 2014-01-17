59dc8016d1b625449ec8c5278d6eab1852faabf5
Zack Youngren <zyoungren@constantcontact.com>
2014-01-17 14:34:08 -0500

    modified all pages and added tests for titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content =>
+      "Magic Ruby Castle of Gemstones and Unicorn Bones")
     end
   end
 
@@ -14,6 +21,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content =>
+      "Contact the Magic Ruby Castle of Gemstones and Unicorn Bones")
+    end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content =>
+      "About the Magic Ruby Castle of Gemstones and Unicorn Bones")
+    end
+  end
 end
