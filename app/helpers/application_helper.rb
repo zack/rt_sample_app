28980d9d9735739d857dc174f33294350db661a4
Zack Youngren <zyoungren@constantcontact.com>
2014-01-22 12:50:47 -0500

    added link specs and moved logo to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    image_tag("logo.jpg", :alt => "Sample App", :class => "round")
+  end
+
   #Return a title on a per-page basis
   def title
     base_title = "Magic Ruby Castle of Gemstones and Unicorn Bones"
