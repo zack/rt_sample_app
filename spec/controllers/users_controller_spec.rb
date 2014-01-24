9fda89e1d18c1ea4f438ac6612d269f7a30d0815
Zack Youngren <zyoungren@constantcontact.com>
2014-01-24 16:46:37 -0500

    finished day of session management

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -69,7 +69,31 @@ describe UsersController do
     end
   end
 
-  describe "Post 'create'" do
+  describe "GET 'edit'" do
+    before do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+
+    it "should show a link to change the Gravatar" do
+      get :edit, :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url,
+                                         :content => "change")
+    end
+  end
+
+  describe "POST 'create'" do
     describe "failure" do
       before(:each) do
         @attr = { :name => "", :email => "", :password => "",
@@ -92,8 +116,7 @@ describe UsersController do
     end
 
     describe "success" do
-
-      before(:each) do
+      before do
         @attr = { :name => "New User", :email => "user@example.com",
                   :password => "foobar", :password_confirmation => "foobar" }
       end
@@ -117,6 +140,89 @@ describe UsersController do
       it "should sign the user in" do
         post:create, :user => @attr
         controller.should be_signed_in
+      end
+    end
+  end
+
+  describe "PUT 'edit'" do
+    before do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    describe "failure" do
+      before do
+        @attr = { :email => "", :name => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "should render the 'edit' page" do
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        put :update, :id => @user, :user => @attr
+        response.should have_selector("title", :content => "Edit user")
+      end
+    end
+
+    describe "succes" do
+      before do
+        @attr = { :name => "New Name", :email => "user@example.org",
+                  :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should change the user's attributes" do
+        put :update, :id => @user, :user => @attr
+        @user.reload
+        @user.name.should  == @attr[:name]
+        @user.email.should == @attr[:email]
+      end
+
+      it "should redirect to the user show page" do
+        put :update, :id => @user, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a flash message" do
+        put :update, :id => @user, :user => @attr
+        flash[:success].should =~ /updated/
+      end
+    end
+  end
+
+  describe "authentication of edit/update pages" do
+    before do
+      @user = Factory(:user)
+    end
+
+    describe "for non-signed-in users" do
+      it "should deny access to 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+      end
+
+      it "should deny access to 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "for signed-in users" do
+      before do
+        wrong_user = Factory(:user, :email => "user@example.net")
+        test_sign_in(wrong_user)
+      end
+
+      it "should require matching users for 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(root_path)
+      end
+
+      it "should require matching users for 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(root_path)
       end
     end
   end
